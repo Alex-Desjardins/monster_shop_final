b7afc7d0bbda2ce5824659195f7bad85fc54a8df
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-19 13:57:40 -0600

    Create "Discounts" link on nav bar. Create merchant discounts index page.

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'Merchant Dashboard' do
     before :each do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'secret')
+      @b_user = @merchant_2.users.create(name: 'Brian', address: '123 Main St', city: 'Boise', state: 'ID', zip: 54321, email: 'brian@example.com', password: 'secret')
       @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
@@ -16,10 +17,14 @@ RSpec.describe 'Merchant Dashboard' do
       @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
       @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
       @order_item_4 = @order_3.order_items.create!(item: @giant, price: @giant.price, quantity: 2, fulfilled: false)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      @discount1 = @merchant_1.discounts.create!(percentage: 5, item_amount: 10)
+      @discount2 = @merchant_1.discounts.create!(percentage: 10, item_amount: 20)
+      @discount3 = @merchant_2.discounts.create!(percentage: 10, item_amount: 5)
+      @discount4 = @merchant_2.discounts.create!(percentage: 10, item_amount: 10)
     end
 
     it 'I can see my merchants information on the merchant dashboard' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
       visit '/merchant'
 
       expect(page).to have_link(@merchant_1.name)
@@ -28,12 +33,14 @@ RSpec.describe 'Merchant Dashboard' do
     end
 
     it 'I do not have a link to edit the merchant information' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
       visit '/merchant'
 
       expect(page).to_not have_link('Edit')
     end
 
     it 'I see a list of pending orders containing my items' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
       visit '/merchant'
 
       within '.orders' do
@@ -56,11 +63,37 @@ RSpec.describe 'Merchant Dashboard' do
     end
 
     it 'I can link to an order show page' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
       visit '/merchant'
 
       click_link @order_2.id
 
       expect(current_path).to eq("/merchant/orders/#{@order_2.id}")
+    end
+
+    it 'I can link to a discounts index page' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      visit '/merchant'
+      expect(page).to have_link "Discounts"
+      click_link "Discounts"
+
+      expect(current_path).to eq("/merchant/discounts")
+      expect(page).to have_content("5% off 10 items")
+      expect(page).to have_content("10% off 20 items")
+    end
+
+    it "Discounts are specific for each merchant" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@b_user)
+      visit '/merchant'
+
+      click_link "Discounts"
+
+      expect(current_path).to eq("/merchant/discounts")
+      expect(page).to have_content("10% off 5 items")
+      expect(page).to have_content("10% off 10 items")
+
+      expect(page).to_not have_content("5% off 10 items")
+      expect(page).to_not have_content("10% off 20 items")
     end
   end
 end
