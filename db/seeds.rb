01e97dfaebaf54d7bca9e7a9407ab45ed9cb26c1
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-22 10:07:50 -0600

    Add discounts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@
 megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
 
+megan.discounts.create!(percentage: 5, items_amount: 5)
+megan.discounts.create!(percentage: 10, items_amount: 20)
+
 user = User.create!(name: 'Billy Joel',
                     address: '123 Song St.',
                     city: 'Las Vegas',
@@ -28,6 +31,7 @@ merchant = User.create!(name: 'Joel Billy',
                         password: '123',
                         merchant_id: megan.id,
                         role: 1)
+
 
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
