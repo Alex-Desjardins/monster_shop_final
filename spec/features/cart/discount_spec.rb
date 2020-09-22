a7ddc2097221a347193daf187e2b9c8dd7d87438
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-22 14:56:30 -0600

    Update discount spec to test greatest discount applied item.

--- spec/features/cart/discount_spec.rb
+++ spec/features/cart/discount_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Cart Discount Show Page' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @megan.discounts.create!(percentage: 5, item_amount: 5)
-      @megan.discounts.create!(percentage: 10, item_amount: 20)
+      @megan.discounts.create!(percentage: 10, item_amount: 10)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 50)
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 25)
 
@@ -20,7 +20,6 @@ RSpec.describe 'Cart Discount Show Page' do
     end
 
     it "I dont see discouts applied if item_amount minimum isnt met" do
-
       within "#item-#{@ogre.id}" do
         expect(page).to have_content('Subtotal: $20.00')
       end
@@ -33,7 +32,6 @@ RSpec.describe 'Cart Discount Show Page' do
     end
 
     it 'I can see item subtotal prices and cart total price reflect applied discounts' do
-
       4.times do
         within "#item-#{@ogre.id}" do
           click_button 'More of This!'
@@ -41,8 +39,7 @@ RSpec.describe 'Cart Discount Show Page' do
       end
 
       within "#item-#{@ogre.id}" do
-        expect(page).to have_content('Subtotal: $95.00')
-        expect(page).to have_content("5% Discount Applied")
+        expect(page).to have_content('Subtotal(5% Discount Applied): $95.00')
       end
 
       within "#item-#{@giant.id}" do
@@ -50,6 +47,24 @@ RSpec.describe 'Cart Discount Show Page' do
       end
 
       expect(page).to have_content('Total: $145.00')
+    end
+
+    it "Cart automatically applies greater discount to items" do
+      9.times do
+        within "#item-#{@ogre.id}" do
+          click_button 'More of This!'
+        end
+      end
+
+      within "#item-#{@ogre.id}" do
+        expect(page).to have_content('Subtotal(10% Discount Applied): $180')
+      end
+
+      within "#item-#{@giant.id}" do
+        expect(page).to have_content('Subtotal: $50.00')
+      end
+
+      expect(page).to have_content('Total: $230.00')
     end
   end
 end
