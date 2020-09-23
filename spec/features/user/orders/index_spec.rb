c88d9eabf9ac51980267e0a71685d78a8b20b218
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-22 19:52:05 -0600

    Add test to display discounted total on orders index page.

--- spec/features/user/orders/index_spec.rb
+++ spec/features/user/orders/index_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'User Order Show Page' do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @sal = Merchant.create!(name: 'Sals Salamanders', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @user = User.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan_1@example.com', password: 'securepassword')
       @order_1 = @user.orders.create!
       @order_2 = @user.orders.create!
-      @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
-      @order_2.order_items.create!(item: @giant, price: @hippo.price, quantity: 2)
-      @order_2.order_items.create!(item: @ogre, price: @hippo.price, quantity: 2)
+      @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2) #40
+      @order_2.order_items.create!(item: @giant, price: @giant.price, quantity: 2) #100
+      @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2) #40
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
@@ -45,6 +45,22 @@ RSpec.describe 'User Order Show Page' do
         expect(page).to have_content("Status: #{@order_2.status}")
         expect(page).to have_content("#{@order_2.count_of_items} items")
         expect(page).to have_content("Total: #{number_to_currency(@order_2.grand_total)}")
+      end
+    end
+
+
+    it 'I see order total changen when items have discounts' do
+      discount_1 = @megan.discounts.create!(percentage: 5, item_amount: 2)
+      # discount_2 = @brian.discounts.create!(percentage: 10, item_amount: 2)
+
+      visit '/profile/orders'
+
+      within "#order-#{@order_1.id}" do
+        expect(page).to have_content("Total: $38.00")
+      end
+
+      within "#order-#{@order_2.id}" do
+        expect(page).to have_content("Total: $133.00")
       end
     end
   end
