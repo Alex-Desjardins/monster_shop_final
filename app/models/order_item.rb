bf86481ed39c61078421c46ee779592064b3ed56
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-22 21:37:17 -0600

    Create #discount_subtotal_of() and #find_item() for item order.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,6 +6,12 @@ class OrderItem < ApplicationRecord
     quantity * price
   end
 
+  def discount_subtotal_of(order_item)
+    item = Item.find(order_item.item_id)
+    greatest_discount = item.greatest_discount(order_item.quantity)
+    (order_item.quantity * item.price * ((100 - greatest_discount.percentage.to_f) / 100)).round(2) if !greatest_discount.nil?
+  end
+
   def fulfill
     update(fulfilled: true)
     item.update(inventory: item.inventory - quantity)
@@ -13,5 +19,9 @@ class OrderItem < ApplicationRecord
 
   def fulfillable?
     item.inventory >= quantity
+  end
+
+  def find_item(order_item_id)
+    Item.find(order_item_id)
   end
 end
