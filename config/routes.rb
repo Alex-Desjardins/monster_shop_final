c24b8fe572df36d1d18a0b2f5499d5c6c3ffd9e2
Alex Desjardins <ardesjardins91@gmail.com>
2020-09-29 11:24:10 -0600

    Refactor Routes; Mod 3 Intermission Work

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/:id/items', to: 'items#index'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy'
 
   resources :items, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
@@ -29,23 +34,41 @@ Rails.application.routes.draw do
   get '/profile/orders/:id', to: 'user/orders#show'
   delete '/profile/orders/:id', to: 'user/orders#cancel'
 
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
 
+  delete '/merchant/orders/:id', to: 'merchants#destroy'
+
   namespace :merchant do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :orders, only: :show
-    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/orders/:id', to: 'orders#show'
+    get '/items', to: 'items#index'
+    get '/items/new', to: 'items#new'
+    post '/items', to: 'items#create'
+    get '/items/:id/edit', to: 'items#edit'
+    patch '/items/:id', to: 'items#update'
+    put '/items/:id', to: 'items#update'
+    delete '/items/:id', to: 'items#destroy'
+    get '/items/:id/change_status', to: 'items#change_status'
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
-    resources :discounts, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/discounts', to: 'discounts#index'
+    get '/discounts/new', to: 'discounts#new'
+    post '/discounts', to: 'discounts#create'
+    get '/discounts/:id/edit', to: 'discounts#edit'
+    patch '/discounts/:id', to: 'discounts#update'
+    put '/discounts/:id', to: 'discounts#update'
+    delete '/discounts/:id', to: 'discounts#destroy'
   end
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    get '/merchants/:id', to: 'merchants#show'
+    patch '/merchants/:id', to: 'merchants#update'
+    get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
     patch '/orders/:id/ship', to: 'orders#ship'
   end
 end
